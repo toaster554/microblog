3307fcb3e7cd599e4f6c55b1016e583b4d5323a6
Nathan Nguyen <btnguyen@umich.edu>
2021-01-21 21:14:57 -0500

    Added export posts functionality

--- microblog.py
+++ microblog.py
@@ -1,5 +1,5 @@
 from app import create_app, db, cli
-from app.models import User, Post, Notification, Message
+from app.models import User, Post, Notification, Message, Task
 
 app = create_app()
 cli.register(app)
@@ -8,4 +8,4 @@ cli.register(app)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
-            'Notifications': Notification}
+            'Notifications': Notification, 'Task': Task}
